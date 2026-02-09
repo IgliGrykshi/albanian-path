fa104414473a5c37683e7b326fe2dd17c26f662d
Igli Grykshi <igli.grykshi@eurostep.it>
2026-02-09 20:41:28 +0100

    sanity tests

--- script.js
+++ script.js
@@ -57,3 +57,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 });
+
+import { createClient } from "https://cdn.skypack.dev/@sanity/client"
+
+export const sanity = createClient({
+  projectId: "pmckc756",
+  dataset: "production",
+  apiVersion: "2026-01-01",
+  useCdn: true
+})
+
+export async function getTours() {
+  return sanity.fetch(`*[_type == "tour"]`)
+}
+// sanity.fetch('*[_type == "tour"]')
+//   .then(console.log)
+//   .catch(console.error)
